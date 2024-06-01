99892397ee4d9768e90c5fcd01268e46a2a32638
DawidKrolikiewicz <dawid.krolikiewicz@student.up.krakow.pl>
2024-06-01 20:53:46 +0200

    pre-merge test

--- CardGame/GameFolder/Code/main.py
+++ CardGame/GameFolder/Code/main.py
@@ -20,8 +20,8 @@ timer = pygame.time.Clock()
 
 PLAYER_NAME = "VictoriousGuy"
 STARTING_HEALTH = 70
-STARTING_DECK = [cardsFile.SecondWind(), cardsFile.Sentinel(), cardsFile.SeverSoul(), cardsFile.Pummel(), cardsFile.Pummel(),
-                 cardsFile.Rage(), cardsFile.BattleTrance(), cardsFile.Inflame(), cardsFile.Disarm()]
+STARTING_DECK = [cardsFile.Strike(), cardsFile.Strike(), cardsFile.Defend(), cardsFile.Defend(), cardsFile.Flex(), cardsFile.Flex(),
+                 cardsFile.TrueGrit(), cardsFile.Havoc(), cardsFile.Clash(), cardsFile.Clash()]
 
 PLAYER = playerFile.Player(PLAYER_NAME, STARTING_HEALTH, STARTING_DECK)
 
